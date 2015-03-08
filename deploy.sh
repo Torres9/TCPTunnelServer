a45bbcde0ca67f5604e9d86c2d2d3bb13bbbae27
Torres9 <Torres9Jin@gmail.com>
2015-03-07 17:01:27 -0800

    minor change about deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 cd $(dirname $0)
-mvn clean
-mvn package
 sudo cp target/TCPTunnelServer.war /usr/share/tomcat8/webapps/ROOT.war
 sudo rm -rf /usr/share/tomcat8/webapps/ROOT
 sudo service tomcat8 restart
