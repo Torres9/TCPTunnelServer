5b413e516f1d80a04ac82b178083d4ee4fde89f2
Torres9 <Torres9Jin@gmail.com>
2015-03-22 20:03:46 -0700

    initial commit for 0.2.0-snapshot

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-cd $(dirname $0)
-#BASE_DIR=/opt/tomcat8
-BASE_DIR=/usr/share/tomcat8
-sudo rm -f $BASE_DIR/logs/TCPTunnelServer.log
-sudo cp target/TCPTunnelServer.war $BASE_DIR/webapps/ROOT.war
-sudo rm -rf $BASE_DIR/webapps/ROOT
-sudo service tomcat8 restart
--- open_log.sh
+++
@@ -1,3 +0,0 @@
-#BASE_DIR=/opt/tomcat8
-BASE_DIR=/usr/share/tomcat8
-sudo tail -f $BASE_DIR/logs/TCPTunnelServer.log
